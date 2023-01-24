54a6d02cd93c1883a6b03df7d61dcea223d628d6
FIRST_NAME LAST_NAME» <andredeidda@hotmail.it>
2023-01-24 23:20:51 +0100

    aggiunto script per far partire i docker-compose

---
+++ docker-compose-up.ps1
@@ -0,0 +1,11 @@
+# change directory to the backend folder
+cd .\backend
+
+# start the backend services using docker-compose
+docker-compose up -d
+
+# change directory to the frontend folder
+cd ..\frontend
+
+# start the frontend services using docker-compose
+docker-compose up -d
