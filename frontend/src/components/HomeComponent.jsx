79e65eb80efab0af182c27490acbc72941d47193
Andrea-Deidda <andredeidda@hotmail.it>
2023-01-27 17:28:50 +0100

    xx

--- frontend/src/components/HomeComponent.jsx
+++ frontend/src/components/HomeComponent.jsx
@@ -7,7 +7,7 @@ log("ciao")
     return (
 
         <div className="container">
-            <h1> Benvenuto nella home !!sdadsadsadsa!!! </h1>
+            <h1> Benvenuto nella home !!!!! </h1>
         </div>
     );
 };
